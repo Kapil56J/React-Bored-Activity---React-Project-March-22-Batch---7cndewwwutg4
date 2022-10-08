fe9eba7f99b7df467f67ae584a4af32241081fd2
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-10-08 19:22:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,66 @@
-import React from 'react'
-import '../styles/App.css';
+// import React from 'react'
+// import '../styles/App.css';
 
-const Loader = () => <div id="loader">Loading...</div>
+// const Loader = () => <div id="loader">Loading...</div>
 
 
-const App = () => {
+// const App = () => {
 
   
 
-  return (
-    <div id="main">
+//   return (
+//     <div id="main">
     
+//     </div>
+//   )
+// }
+
+
+// export default App;
+
+import '../styles/App.css';
+import React,{useState, useEffect} from 'react';
+
+const Loader = () => <div id="loader">Loading...</div>
+
+const App = () => {
+  const [data,setData] = useState("");
+  const [loading,setLoading] = useState(true)
+ 
+//   const makeURL = async (type) =>{
+//     const data = await fetch(`https://www.boredapi.com/api/activity?type=${type}`)
+//     const response = await data.json();
+//     setLoading(false)
+//     setData(response.activity)
+//   }
+const makeURL = (type) => `http://www.boredapi.com/api/activity?type=${type}`
+  const makeCall = (type) => {
+    fetch(makeURL(type)
+    ).then((resp)=>{
+      return resp.json()}).then((dat)=>{
+      setLoading(false);
+      setData(dat.activity);
+    })
+  };
+  
+  useEffect(()=>{
+    makeCall("education")
+  },[])
+
+  const handleR =()=>{
+    setLoading(true)
+    makeCall("recreational")
+  }
+  const handleE =()=>{
+    setLoading(true)
+    makeCall("education")
+  }
+  return (
+    <div id="activity">
+      {loading ? Loader() : data}
+      <br />
+      <button id="btn-recreation" onClick={handleR}>Recreational</button>
+      <button id="btn-education" onClick={handleE}>Education</button>
     </div>
   )
 }
